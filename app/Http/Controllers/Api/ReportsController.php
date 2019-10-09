1e608606810893239b6935bb1eca1158422c036c
florenmoya <florenmoya17@gmail.com>
2019-10-09 21:27:24 +0800

    Beta 10.2

--- app/Http/Controllers/Api/ReportsController.php
+++ app/Http/Controllers/Api/ReportsController.php
@@ -32,9 +32,9 @@ class ReportsController extends Controller
         $today = Carbon::today();
         $top_product = $items->with(array('Item'=>function($query){
         $query->with('Category');
-        }))->selectRaw('SUM(quantity) AS total_quantity, item_id')->groupBy('item_id')->orderByRaw('SUM(quantity) DESC')->limit(3)->get();
+        }))->selectRaw('SUM(quantity) AS total_quantity, item_id')->groupBy('item_id')->orderByRaw('SUM(quantity) DESC')->limit(10)->get();
 
-        $restock = Item::with('Category')->where('quantity', '<=', 3)->orderBy('quantity', 'DESC')->limit(5)->get();
+        $restock = Item::with('Category')->where('quantity', '<=', 3)->orderBy('quantity', 'DESC')->get();
 
         $month_bread_delivery = DeliveriesItem::selectRaw('SUM(price) AS total_price')->whereHas('Item', function($q){$q->where('category_id', 1);})->whereDate('created_at', Carbon::today())->get();
         $today_sales = $items->selectRaw('SUM(price) as total_price')->whereDate('created_at', Carbon::today())->get();
