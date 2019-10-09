7c3bd9368e24ce9d5c1994447942c6c487949fc5
florenmoya <florenmoya17@gmail.com>
2019-10-09 21:35:24 +0800

    Beta 10.2

--- app/Http/Controllers/Api/ReportsController.php
+++ app/Http/Controllers/Api/ReportsController.php
@@ -32,11 +32,11 @@ class ReportsController extends Controller
         $today = Carbon::today();
         $top_product = $items->with(array('Item'=>function($query){
         $query->with('Category');
-        }))->selectRaw('SUM(quantity) AS total_quantity, item_id')->groupBy('item_id')->orderByRaw('SUM(quantity) DESC')->limit(10)->get();
+        }))->selectRaw('SUM(quantity) AS total_quantity, item_id')->groupBy('item_id')->orderByRaw('SUM(quantity) DESC')->whereMonth('created_at', Carbon::today())->get->limit(10)->get();
 
-        $restock = Item::with('Category')->where('quantity', '<=', 3)->orderBy('quantity', 'ASC')->get();
+        $restock = Item::with('Category')->where('quantity', '<=', 3)->orderBy('quantity', 'ASC')->whereMonth('created_at', Carbon::today())->get();
 
-        $month_bread_delivery = DeliveriesItem::selectRaw('SUM(price) AS total_price')->whereHas('Item', function($q){$q->where('category_id', 1);})->whereDate('created_at', Carbon::today())->get();
+        $month_bread_delivery = DeliveriesItem::selectRaw('SUM(price) AS total_price')->whereHas('Item', function($q){$q->where('category_id', 1);})->->whereMonth('created_at', Carbon::today())->get();
         $today_sales = $items->selectRaw('SUM(price) as total_price')->whereDate('created_at', Carbon::today())->get();
         $month_revenue = $items->selectRaw('SUM(price) as total_price')->whereMonth('created_at', Carbon::today())->get();
         $dashboard = array('month_bread_delivery' => $month_bread_delivery[0]->total_price, 'today_sales' => $today_sales[0]->total_price, 'month_revenue' => $month_revenue[0]->total_price, 'top_product' => $top_product, 'restock' => $restock);
